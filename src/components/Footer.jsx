6eef5ebe1381331c2e730df21863ea757b9bde08
Limbert Emanuel Molina Intriago <limbermolina9596@gmail.com>
2026-01-01 20:23:41 -0500

    updated-footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
-import '../styless/Footer.css'
+import "../styless/Footer.css";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      <p>© 2025 Limbert Molina – Todos los derechos reservados.</p>
+      <p>
+        © {new Date().getFullYear()} Limbert Molina – Todos los derechos
+        reservados.
+      </p>
     </footer>
-  )
+  );
 }
